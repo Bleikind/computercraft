d4b90afc1fbb6f98aa8ba0e76902b0e94df9bd36
Paul <info@bleikind.de>
2021-06-05 15:30:41 +0200

    fix

---
+++ Server/telemetryclient.lua
@@ -0,0 +1,55 @@
+local protocol = "telemetry"
+local hostname = "mainsrv"
+
+function split(input, seperator)
+    if seperator == nil then
+        sep = "/"
+    end
+
+    local t={}
+    for str in string.gmatch(input, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+
+    return t
+end
+
+function tableLength(t)
+    local count = 0
+    for _ in pairs(t) do
+        count = count + 1
+    end
+
+    return count
+end
+
+
+rednet.open("back")
+srvID = rednet.lookup(protocol, hostname)
+
+rednet.send(srvID, "cmd_client_connect")
+response = rednet.receive(protocol, 10)
+
+if response then
+    if response == "logged_in" then
+        print("Logged in.")
+    else
+        print("Already logged in?")
+    end
+else
+    print("No response from server.")
+end
+
+while true do
+    senderId, message, proto = rednet.receive(protocol)
+
+    strings = split(message, ' ')
+    cmd = strings[0]
+
+    if cmd == "cmd_message" then
+        messageFrom = strings[1]
+        msg = table.concat(strings, " ", 2, tableLength(strings))
+
+        print(messageFrom .. ': ' .. msg)
+    end
+end
--- Server/telemetryserver.lua
+++ Server/telemetryserver.lua
@@ -1,10 +1,8 @@
-local modem = peripheral.find('modem')
 local protocol = "telemetry"
 local hostname = "mainsrv"
+local side = "back"
 
-if not modem then
-    error('Kein Modem an dem Ding')
-end
+local clients = {}
 
 function split(input, seperator)
     if seperator == nil then
@@ -28,28 +26,77 @@ function tableLength(t)
     return count
 end
 
+function containsClient(id)
+    for index, value in ipairs(clients) do
+        if value == id then
+            return index
+        end
+    end
+
+    return nil
+end
+
+function removeClient(id)
+    index = containsClient(id)
+    if index then
+        table.remove(clients, index)
+        return true
+    end
+
+    return false
+end
+
+function addClient(id)
+
+    index = containsClient(id)
+
+    if not index then
+        table.insert(clients, id)
+        return true
+    end
+
+    return false
+end
+
 function start()
     if rednet.lookup(protocol, hostname) then
         error('Hostname wird bereits verwendet')
         return
     end
 
+    rednet.open(side)
     rednet.host(protocol, hostname)
 
     while true do
-        e, sender, message, distance = os.pullEvent("rednet_message")
+        senderId, message, proto = rednet.receive(protocol)
 
         strings = split(message, ' ')
 
         if(strings[0] == 'cmd_broadcast') then
             local msg = string.sub(message, 13, string.len(message))
-            rednet.broadcast('cmd_message ' .. sender .. ' ' .. msg, protocol)
-            print(sender .. ': ' .. msg)
+
+            for index, value in ipairs(clients) do
+                rednet.send(value, 'cmd_message' .. senderId .. ' ' .. msg, protocol)
+            end
+
+            print(senderId .. ': ' .. msg)
+        end
+
+        if strings[0] == "cmd_client_connect" then
+            print("Client connecting...")
+
+            if addClient(senderId) then
+                rednet.send(senderId, "logged_in", protocol)
+                print(senderId .. " connected.")
+            else
+                rednet.send(senderId, "already_logged_in", protocol)
+                print(senderId .. "already conntected.")
+            end
         end
 
     end
 
-    modem.close()
+    rednet.close(side)
 end
 
 start()
