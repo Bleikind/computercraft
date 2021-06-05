ffc90e98d9f7dfb05e936f2dadeb9096bbf90667
Paul <info@bleikind.de>
2021-06-05 11:37:36 +0200

    fix

--- Turtle/harvest.lua
+++ Turtle/harvest.lua
@@ -1,19 +1,52 @@
-function checkFuel() 
+function refuel()
     for i = 1, 16 do
         selected_slot = turtle.select(i)
         if selected_slot then
             local data = turtle.getItemDetail();
 
-            if data == not nil then
-                if data.name == "minecraft:coal" then
-                    turtle.refuel(turtle.getItemCount())
+            if data then
+                if turtle.refuel() then
                     print('Fuel state: ' .. turtle.getFuelLevel() .. " / " .. turtle.getFuelLimit())
                 end
             end
-        else
-
         end
     end
 end
 
-checkFuel()
+function harvest(item_name)
+    local inspect = turtle.inspect()
+
+    if(inspect) then
+        if inspect.name == item_name then
+            if inspect.state.age == 7 then
+                turtle.dig()
+                turtle.place()
+            end
+        end
+    end
+end
+
+function checkRight(item_name)
+    turtle.turnRight()
+    harvest(item_name)
+    turtle.turnLeft()
+end
+
+function checkWater()
+    return turtle.inspectDown().name == "minecraft:water"
+end
+
+function go(item_name)
+    while checkWater() do
+        checkRight(item_name)
+    end
+
+    turtle.back()
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+
+refuel()
+go()
+
