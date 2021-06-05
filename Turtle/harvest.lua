a5df13d0ebe8beb7116025e555f63834d7207cb6
Paul <info@bleikind.de>
2021-06-05 10:29:26 +0200

    main

---
+++ Turtle/harvest.lua
@@ -0,0 +1,10 @@
+function checkFuel() 
+    for i = 1, 16 do
+        selected = turtle.select(i)
+        if selected then
+            print(selected .. ' ' .. i)
+        else
+
+        end
+    end
+end
