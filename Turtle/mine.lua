af73edf7957ec0c1671a0538e86d861f055d16bc
Paul <info@bleikind.de>
2021-06-06 13:25:35 +0200

    fix

--- Turtle/mine.lua
+++ Turtle/mine.lua
@@ -21,16 +21,16 @@ function setStartPosition()
 end
 
 function go()
+    turnDirection = 1
 
     for i = 1, width, 1 do
-        turnDirection = 1
 
         for x = 1, width, 1 do
             turtle.digDown()
             turtle.forward()
         end
 
-        if turnDirection % 2 then
+        if turnDirection % 2 == 1 then
             turtle.turnRight()
             turtle.forward()
             turtle.turnRight()
@@ -40,10 +40,10 @@ function go()
             turtle.turnLeft()
         end
 
-        if(turnDirection == 1) == 1 then
-            turnDirection = 2
-        else
+        if turnDirection == 1 then
             turnDirection = 1
+        else
+            turnDirection = 2
         end
     end
 
@@ -52,9 +52,9 @@ function go()
     turtle.turnRight()
 
     if(turnDirection == 1) == 1 then
-        turnDirection = 2
-    else
         turnDirection = 1
+    else
+        turnDirection = 2
     end
 end
 
